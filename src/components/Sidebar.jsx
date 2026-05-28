f51e9dfc919a2919454a953bdaeb4b373a21c0e0
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-05-29 03:07:21 +0530

    fix some spacing issue for desktop and mobile layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,14 +3,14 @@ import { sidebarLinks } from "../constants/sidebarData"
 
 const Sidebar = () => {
     return (
-        <div className="flex flex-col items-center justify-between p-4 md:h-screen w-full md:w-64 border bg-zinc-900 text-white border-r border-zinc-800">
+        <div className="flex flex-col p-4 md:h-screen w-full md:w-64 border bg-zinc-900 text-white border-r border-zinc-800">
             <div className="font-bold text-lg mb-8 pb-4 border-b border-zinc-800 ">Nova<span className="italic text-blue-500 ">Desk</span></div>
-            <div className="flex flex-row md:flex-col gap-3 justify-between">
+            <div className="flex flex-row md:flex-col gap-3 mt-8">
 
                 {sidebarLinks.map((data) => (
                     <NavLink
                         to={data.path}
-                        key={data.path}
+                        key={data.path} 
                         className={({ isActive }) =>
                             isActive
                                 ? "px-3 py-2 md:px-4 md:py-3 rounded-lg bg-zinc-800 text-white"
